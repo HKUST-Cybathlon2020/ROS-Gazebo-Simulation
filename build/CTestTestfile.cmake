2b37fbb79a70400564e1d6db7146d103373d15c4
hkust-exoskeleton <kftsang@connect.ust.hk>
2019-06-04 11:25:19 +0800

    add friction

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/admin/catkin_ws/src/exoskeleton
-# Build directory: /home/admin/catkin_ws/src/exoskeleton/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
--- build/gtest/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/admin/catkin_ws/src/exoskeleton/build/gtest/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
